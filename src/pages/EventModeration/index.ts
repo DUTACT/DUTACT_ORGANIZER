106dbd0154ff0e3d50c659253e5f4bf4eb652231
Sang Tấn <sangprovai14@gmail.com>
2024-11-28 20:39:39 +0700

    remove redundant components (#60)

--- src/pages/EventModeration/index.ts
+++ src/pages/EventModeration/index.ts
@@ -1,5 +1,4 @@
-import EventModeration from './EventModeration'
 import ModeratedEvent from './ModeratedEvent'
 import PendingEvent from './PendingEvent'
 
-export { EventModeration, ModeratedEvent, PendingEvent }
+export { ModeratedEvent, PendingEvent }
